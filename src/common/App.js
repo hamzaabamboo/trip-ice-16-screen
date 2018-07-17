8cd4f970db2f897588cb684024ddb51b8e39e1a4
Tanyawat Vittayapalotai <hamzaabamboo@gmail.com>
2018-07-18 00:59:27 +0700

    :truck: Move code around

---
+++ src/common/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Router from './Router';
+import { BrowserRouter } from 'react-router-dom';
+class App extends Component {
+	render() {
+		return (
+			<BrowserRouter>
+				<Router />
+			</BrowserRouter>
+		);
+	}
+}
+
+export default App;
