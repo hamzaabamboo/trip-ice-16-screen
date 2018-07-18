8d56c771ebe927760cba1af33aff85e473319bd8
Tanyawat Vittayapalotai <hamzaabamboo@gmail.com>
2018-07-18 15:15:23 +0700

    :sparkles: Adding privacy policy because Facebook wants it

--- src/common/Router.js
+++ src/common/Router.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Controls from '../controls';
+import Privacy from '../privacy';
 import Home from '../home';
 
 export default class Router extends Component {
@@ -8,8 +9,8 @@ export default class Router extends Component {
 		return (
 			<Switch>
 				<Route path="/controls" component={Controls} />
-				<Route exact path="/" component={Home} />
-				{/* <Route component={NoMatch} /> */}
+				<Route path="/privacy" component={Privacy} />
+				<Route path="/" component={Home} />
 			</Switch>
 		);
 	}
---
+++ src/privacy/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+class App extends React.PureComponent {
+	render() {
+		return (
+			<div>
+				We only use facebook for Admin Auth, nothing to worry here.
+				Adding this because facebook wants it.
+			</div>
+		);
+	}
+}
+
+export default App;
