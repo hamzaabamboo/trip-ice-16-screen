e6da386d221ae11fba504e6687eff7340de414d3
Tanyawat Vittayapalotai <hamzaabamboo@gmail.com>
2018-07-18 12:21:00 +0700

    :card_file_box: changed real-time database to firestore

--- src/firebase.js
+++ src/firebase.js
@@ -5,4 +5,5 @@ firebase.initializeApp(config);
 
 export const ref = firebase.database().ref();
 export const auth = firebase.auth;
+export const db = firebase.firestore();
 export const provider = new firebase.auth.FacebookAuthProvider();
