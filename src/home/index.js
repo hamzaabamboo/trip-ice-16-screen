7445754cc818060d7eaaaeebf0b96f8a528b47bc
Tanyawat Vittayapalotai <hamzaabamboo@gmail.com>
2018-07-19 17:47:57 +0700

    :lipstick: trippy background update

--- src/home/index.js
+++ src/home/index.js
@@ -45,53 +45,75 @@ export default Home;
 const Background = styled.div`
 	height: 100vh;
 	width: 100vw;
-	background: linear-gradient(216deg, #0000ff, #add8e6, #00ffff);
-	background-size: 600% 600%;
+	background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
+	background-size: 400% 400%;
 	display: flex;
 	flex-direction: row;
 	justify-content: center;
-	-webkit-animation: AnimationName 26s ease infinite;
-	-moz-animation: AnimationName 26s ease infinite;
-	animation: AnimationName 26s ease infinite;
+	-webkit-animation: Gradient 20s ease infinite;
+	-moz-animation: Gradient 20s ease infinite;
+	animation: Gradient 20s ease infinite;
 
-	@-webkit-keyframes AnimationName {
+	@-webkit-keyframes Gradient {
 		0% {
-			background-position: 87% 0%;
+			background-position: 0% 50%;
 		}
 		50% {
-			background-position: 14% 100%;
+			background-position: 100% 50%;
 		}
 		100% {
-			background-position: 87% 0%;
+			background-position: 0% 50%;
 		}
 	}
-	@-moz-keyframes AnimationName {
+
+	@-moz-keyframes Gradient {
 		0% {
-			background-position: 87% 0%;
+			background-position: 0% 50%;
 		}
 		50% {
-			background-position: 14% 100%;
+			background-position: 100% 50%;
 		}
 		100% {
-			background-position: 87% 0%;
+			background-position: 0% 50%;
 		}
 	}
-	@keyframes AnimationName {
+
+	@keyframes Gradient {
 		0% {
-			background-position: 87% 0%;
+			background-position: 0% 50%;
 		}
 		50% {
-			background-position: 14% 100%;
+			background-position: 100% 50%;
 		}
 		100% {
-			background-position: 87% 0%;
+			background-position: 0% 50%;
 		}
 	}
 `;
 const Title = styled.h1`
-	font-size: 4em;
 	text-align: center;
-	margin: 20px 0;
+	font-size: 4em;
+	background: linear-gradient(
+		to right,
+		#01579b 20%,
+		#29b6f6 40%,
+		#29b6f6 60%,
+		#01579b 80%
+	);
+	background-size: 200% auto;
+
+	color: #000;
+	background-clip: text;
+	text-fill-color: transparent;
+	-webkit-background-clip: text;
+	-webkit-text-fill-color: transparent;
+
+	animation: shine 10s linear infinite;
+	@keyframes shine {
+		to {
+			background-position: 200% center;
+		}
+	}
 `;
 const Content = styled.div`
 	display: flex;
